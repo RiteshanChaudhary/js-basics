603f01de1cf28c8baca50be10f9cf6f170ae1323
Riteshan Chaudhary <riteshanchau15@gmail.com>
2025-02-09 14:30:29 +0545

    add: Array Js

---
+++ 8.array.js
@@ -0,0 +1,21 @@
+// Javascript arrays
+// const numbers = [1,2,3,4]
+
+// const persons = ["Shyam", "Hari", "Ram", "Sita"];
+
+// console.log(persons);
+// console.log(persons.length);
+// persons.push("Lal");
+// persons.pop();
+// persons.shift();
+// persons.unshift("Lal");
+// console.log(persons.at(-1), persons[persons.length - 1], persons[3]);
+
+// const numbers = [1, 2, 3, 4];
+// Map
+// const newArr = numbers.map((el, i) => el * 2);
+
+//filter
+// const filterArr = numbers.filter((el, i) => i !== 2);
+// console.log(numbers);
+// console.log(filterArr);
