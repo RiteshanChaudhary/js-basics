3fd45edf60b3e6d98e2ba66c55e0776a00048dfd
Riteshan Chaudhary <riteshanchau15@gmail.com>
2025-02-10 14:00:17 +0545

    add: data types js

---
+++ 2.datatypes.js
@@ -0,0 +1,23 @@
+// data types
+// type of data that variable can store
+
+//string
+//number
+//boolean
+//undefined
+//null
+//object
+//symbol
+//bigInt
+
+//? string
+// "" (double quote)
+// '' (single quote)
+// `` (back=tick)
+// let x= `123`;
+// console.log(x,typeof x);
+
+//? number
+// ? NaN => Not a Number ("nepal"/25)
+//let num = 25;
+//console.log(num, typeof num);
